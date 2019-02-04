1bdbb8e292f9ce916c5bb47c9be87b3aca4ce096
Alan R. Pearse <alan.pearse@connect.qut.edu.au>
2019-02-04 11:48:23 +1000

    Adding package installation scripts for users who want to run these locally

---
+++ installing_packages_for_geoprocessing.R
@@ -0,0 +1,5 @@
+### Run this command to install the R packages needed to run 'shiny_app_geoprocessing.R'
+
+install.packages(
+  c("rgdal", "raster", "rgeos", "sp", "zip", "shiny")
+)
---
+++ installing_packages_for_suitability_susceptibility.R
@@ -0,0 +1,5 @@
+### Run this command to install the R packages needed to run 'shiny_app_suitability.R' and 'shiny_app_susceptibility.R'
+
+install.packages(
+  c("rgdal", "raster", "rgeos", "sp", "zip", "shiny", "dplyr", "truncnorm", "stringr", "visNetwork")
+)
