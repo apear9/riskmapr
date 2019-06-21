85e3071121f6b3343ccca4aefcb31bddf4aa9f30
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-06-22 00:20:36 +1000

    Mistakenly used old version of file

--- sensitivity_analysis/numerical_state_functions.R
+++ sensitivity_analysis/numerical_state_functions.R
@@ -6,8 +6,10 @@ expand_grid <- function(n_var, numerical_states = seq(0, 100, 25)){
   # Grid expansion
   code_block <- "expand.grid("
   additions <- "V"
-  for(i in 1:(n_var - 1)){
-    additions <- paste(additions, "V", sep = ", ")
+  if(n_var > 1){
+    for(i in 1:(n_var - 1)){
+      additions <- paste(additions, "V", sep = ", ")
+    }
   }
   code_block <- paste0(code_block, additions, ")") # close bracket
   
@@ -21,16 +23,22 @@ cut_down_grid <- function(grid){
   # Input checking
   nc <- ncol(grid)
   nr <- nrow(grid)
-  if(nc < 2) stop("Grid must be a data.frame or matrix with > 1 column.")
-  
-  # Cut down grid
-  ind <- rep(TRUE, nr)
-  for(i in 2:nc){
-    ind <- ind & (unlist(grid[, i-1]) < unlist(grid[, i]))
+  if(nc > 1){
+    
+    # Cut down grid
+    ind <- rep(TRUE, nr)
+    for(i in 2:nc){
+      ind <- ind & (unlist(grid[, i-1]) < unlist(grid[, i]))
+    }
+    
+    # Result
+    return(grid[ind, ])
+    
+  } else {
+    
+    return(grid)
+    
   }
-  
-  # Result
-  return(grid[ind, ])
   
 }
 
@@ -43,17 +51,17 @@ replace <- function(r, grid){
   # Input checking
   nr <- nrow(grid)
   nc <- ncol(grid)
-  if(nc != uv) stop("The number of unique values in the raster r must match the number of of columns in grid.")
+  if(nc != length(uv)) stop("The number of unique values in the raster r must match the number of of columns in grid.")
   
   # Replace raster values
   file_original <- filename(r)
   file_appendages <- paste0("_Grid_Levels_", 1:nr, ".tif")
   for(i in 1:nr){
-    file_new <- gsub(".tif", file_appendages, file_original)
+    file_new <- gsub(".tif", file_appendages[i], file_original)
     for(j in 1:nc){
       r_copy[r[] == uv[j]] <- unlist(grid[i, j])
     }
-    writeRaster(r_copy, file_new)
+    writeRaster(r_copy, file_new, overwrite = TRUE)
   }
   
   # When done, return nothing
